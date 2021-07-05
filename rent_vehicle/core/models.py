48713887ef2b153960a4fd31012bceee271e979c
Przemek Pyrka <przemek_dei@op.pl>
2021-07-05 22:49:05 +0200

    "reverting test changes"

--- rent_vehicle/core/models.py
+++ rent_vehicle/core/models.py
@@ -33,7 +33,6 @@ class Vehicle(models.Model):
 
 
 class Rent(models.Model):
-    ala = alaq
     MAX_RENT_TIME = timedelta(days=7)
     LAST_AVAILABLE_TIME = timedelta(days=90)
     started_at = models.DateTimeField()
